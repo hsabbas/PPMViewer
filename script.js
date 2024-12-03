e15f4e91bf1b0ce34e74cb940bd9dcfef4ced7b7
Hassan <habbas375@gmail.com>
2024-12-02 21:47:37 -0600

    Completed file parsing

--- script.js
+++ script.js
@@ -10,69 +10,80 @@ uploadInput.addEventListener("change", () => {
     const file = uploadInput.files[0];
     if (!file) {
         error.innerText = "Failed to read file";
-        clearCanvas();
         return;
     }
 
     if (!file.name.endsWith(".ppm")) {
         error.innerText = "Not a .ppm file";
-        clearCanvas();
         return;
     }
 
-    error.innerText = "";
-    parseFile(file);
-    drawCanvas();
-})
-
-function parseFile(file) {
     const reader = new FileReader();
     reader.onload = function () {
         const content = reader.result.trim();
-        let parsedWords = [];
-
-        let i = 0;
-        while (i < content.length) {
-            let line = "";
-            while (i < content.length &&
-                (content[i] !== '\n' && content[i] !== '#' && content[i] !== '\r')) {
-                line += content[i];
-                i++;
-            }
-            parsedWords = parsedWords.concat(line.split(' '));
-            if(i < content.length && content[i] === '#'){
-                while(i < content.length && content[i] !== '\n'){
-                    i++;
-                }
-            }
-            while (i < content.length && (content[i] === '\n' || content[i] === '\r')) {
-                i++;
-            }
-        }
-
-        if (parsedWords[0] !== "P3") {
-            error.innerText = "PPM file format is invalid";
-            return;
-        }
-        width = parseInt(parsedWords[1]);
-        if (isNaN(width)) {
-            error.innerText = "Failed to parse width";
-            return;
-        }
-        height = parseInt(parsedWords[2]);
-        if (isNaN(height)) {
-            error.innerText = "Failed to parse height";
-            return;
-        }
-        if (parsedWords[3] !== "255") {
-            error.innerText = "Max color value must be 255";
-            return;
+        const parsedWords = parseFile(content);
+        const valid = validatePPMFile(parsedWords);
+        if(valid) {
+            clearCanvas();
+            drawCanvas(parsedWords);
         }
     }
     reader.readAsText(file);
+})
+
+function parseFile(content) {
+    let parsedWords = [];
+
+    let i = 0;
+    while (i < content.length) {
+        let line = "";
+        while (i < content.length &&
+            (content[i] !== '\n' && content[i] !== '#' && content[i] !== '\r')) {
+            line += content[i];
+            i++;
+        }
+        parsedWords = parsedWords.concat(line.trim().split(' '));
+        if(i < content.length && content[i] === '#'){
+            while(i < content.length && content[i] !== '\n'){
+                i++;
+            }
+        }
+        while (i < content.length && (content[i] === '\n' || content[i] === '\r')) {
+            i++;
+        }
+    }
+    console.log(parsedWords);
+    
+    return parsedWords;
 }
 
-function drawCanvas() {
+function validatePPMFile(parsedWords) {
+    if (parsedWords[0] !== "P3") {
+        error.innerText = "PPM file format is invalid";
+        return false;
+    }
+    width = parseInt(parsedWords[1]);
+    if (isNaN(width)) {
+        error.innerText = "Failed to parse width";
+        return false;
+    }
+    height = parseInt(parsedWords[2]);
+    if (isNaN(height)) {
+        error.innerText = "Failed to parse height";
+        return false;
+    }
+    if (parsedWords[3] !== "255") {
+        error.innerText = "Max color value must be 255";
+        return false;
+    }
+    if(parsedWords.length !== 4 + width * height * 3){
+        error.innerText = "PPM file format is invalid";
+        return false;
+    }
+    return true;
+}
+
+function drawCanvas(parsedWords) {
     const width = 255;
     const height = 255;
     canvas.setAttribute("width", width);
